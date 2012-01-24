e95093322eb2c78f40473e4c05f54bf231083c39
Your Name <i@sdo.bz>
2012-01-23 18:34:11 -0800

    Appcontrol work

--- ramonast/appcontrol.py
+++ ramonast/appcontrol.py
@@ -3,8 +3,7 @@
 # As of 12/17/11 this module does NOT work, and doesn't conform to RamonaSt
 # module format. Will be updated later --sdobz
 
-import os, subprocess, time
-from random import random
+import os, subprocess, time, library.rs_process as rs_process
 
 commands={}
 port = 7979
@@ -23,150 +22,41 @@ def action(app,action,block=False):
 		return "Invalid app"
 	if not (action in actions):
 		return "Invalid action"
-	
+	if(app+action in commands):
+		return "Command already running"
+	args=["sudo","control",app,action]
 	if(block):
-		return runcmd(["sudo","control",app,action])
+		return runcmd(args)
 	else:
 		log("Responding...")
-		prg = 
+		commands[app+action] = rs_process.process(args,60,True)
+		
 def rcmd(app,action):
 	if not (app in apps):
 		return "Invalid app"
 	if not (action in actions):
 		return "Invalid action"
-	
-	pid = daemon()
-	if not(pid):
+	if not(app+action in commands):
 		return "Command not running"
+	prg=commands[app+action]
+	output=prg.read()
+	if(len(output)!=0):
+		return output
+	else:
+		if not(prg.running()):
+			del commands[app+action]
+			return "#DONE#"
 	
-	return xmlrpcclient().rcmd(app,action)
+	return ""
 	
 def runcmd(args):
 	# Run the args and return the output
 	return subprocess.check_output(args)
 
-def getresponse(pid,func,app,action):
-	# Write to the stdin of the daemon
-	with open("/proc/"+pid+"/fd/0","w") as f:
-		f.write(func+" "+app+" "+action)
-	
-	# Read the ensuing output
-	with open("/proc/"+pid+"/fd/1","r") as f:
-		return f.read()
-	
 def running(app):
 	if("RUNNING" in action(app,"status",block=True)):
 		return True
 	return False
 
-def startup(app):
-	if("YES" in action(app,"checkstartup",block=True)):
-		return True
-	return False
-
 def log(text):
 	print(text)
-
-class command:
-	def __init__(self,app,action):
-		global commands
-		if app+action in commands:
-			return False
-		
-		self.proc=self.makeprocess(app,action)
-		
-	def makeprocess(self,app,action):
-		return Process(["sudo","control",app,action])
-	
-if(__name__=="__main__"):
-	import sys
-	from asyncproc import Process
-	from select import select
-	
-	def datawaiting(socket):
-		return select([socket], [], [], .1) == ([socket], [], [])
-	
-	def xmlaction(app,action):
-		log("Action! "+app+action)
-		if(app+action in running):
-			return "Command already running"
-		else:
-			running[app+action]={
-				'proc':makeprocess(app,action),
-				'buffer':'',
-				'last':int(time.time()),
-				'done':False
-			}
-			return "Command started"
-	def xmlrcmd(app,action):
-		log("RCMD! "+app+action)
-		try:
-			program = running[app+action]
-			output = program['buffer']
-			program['buffer'] = ''
-			if(program['done']):
-				output+="#DONE#"
-				del running[app+action]
-			return output
-		except KeyError:
-			return "Command not running"
-	
-	# Restrict to a particular path.
-	class RequestHandler(SimpleXMLRPCRequestHandler):
-		rpc_paths = ('/RPC2',)
-
-	# Create server
-	server = SimpleXMLRPCServer(("localhost", port),requestHandler=RequestHandler)
-	# server.register_introspection_functions()
-
-
-	server.register_function(xmlaction, 'action')
-	server.register_function(xmlrcmd,   'rcmd')
-	
-	
-	
-	# This informs the action function that it is running
-	log("Running")
-
-		
-	running={}
-	while(True):
-		if(datawaiting(server.fileno())):
-			log("Handled request.")
-			server.handle_request()
-		
-		expired=[]
-		for context in running:
-			program=running[context]
-			# check to see if process has ended
-			poll = program['proc'].wait(os.WNOHANG)
-			if(poll == None):
-				# It hasn't ended, update it's last seen time
-				program['last']=int(time.time())
-			else:
-				program['done']=True
-				if(int(time.time())-program['last'] > 20):
-					# Been too long after it ended, destroy the buffer
-					log("Program "+context+" expired.")
-					# Mark it for deletion
-					expired.append(context)
-					# Don't attempt to read it's shit, it's over
-					continue
-			
-			# store any new output
-			out = program['proc'].read()
-			if out != "":
-				log("Program "+context+" outputted "+out)
-				program['buffer']+=out
-		
-		for item in expired:
-			del running[item]
-		
-		if(len(running)==0):
-			log("All programs expired, quitting")
-			# Remove the pid...
-			try:
-				os.remove('/var/run/applications/applications.pid')
-			except OSError:
-				pass
-			break
--- ramonast/library/rs_process.py
+++ ramonast/library/rs_process.py
@@ -1,46 +1,112 @@
 #!/usr/bin/env python
-import os, subprocess
-from pprint import pprint
+import os, sys, subprocess
+from threading import Timer
+from threading import Thread
+from Queue import Queue, Empty
 from time import sleep
 from time import time
-from sys import stderr
 
 processes=[]
+EXPIRE_RATE=1
+ON_POSIX = 'posix' in sys.builtin_module_names
 
 # These are compiled at the end of the file
 class process:
-	def __init__(self,args=()):
+	def __init__(self,args=(),expiretime=60,buffered=False):
 		global processes
 		self.seen()
 		self.args=args
 		self.proc=False
 		processes.append(self)
-		self.offset=0
-	
-	def makeprocess(self):
-		self.kill()
-		self.proc=subprocess.Popen(self.args,stderr=stderr,stdout=subprocess.PIPE)
+		self.expiretime=expiretime
 		
-	def getchunk(self,size):
-		self.seen()
-		if(self.running()):
-			chunk=self.proc.stdout.read(size)
-			self.offset+=len(chunk)
-			return chunk
-	
-	def running(self):
-		if(self.proc):
-			return True
+		if(buffered):
+			# I dunno how this works, really, but whatever.
+			# http://stackoverflow.com/questions/375427/non-blocking-read-on-a-subprocess-pipe-in-python
+			self.buffer=""
+			self.proc=subprocess.Popen(self.args,stdout=subprocess.PIPE, bufsize=1, close_fds=ON_POSIX)
+			self.queue=Queue()
+			self.thread=Thread(target=enqueue_output, args=(self.proc.stdout, self.queue))
+			self.thread.daemon=True
+			self.thread.start()
 		else:
-			return False
+			self.buffer=False
+			self.proc=subprocess.Popen(self.args,stdout=subprocess.PIPE, close_fds=ON_POSIX)
+		
+		check_expired()
 	
-	def kill(self):
+	def __del__(self):
 		if(self.proc):
 			self.proc.kill()
+			self.proc.wait()
+	
+	def read(self,size=0):
+		self.seen()
+		if(self.proc):
+			if(self.buffer!=False):
+				# One last read
+				self.expired()
+				if(size==0):
+					size = len(self.buffer)
+				chunk=self.buffer[:size]
+				self.buffer=self.buffer[size:]
+				return chunk
+			else:
+				return self.proc.stdout.read(size)
 		
+	
+	def running(self):
+		return program['proc'].wait(os.WNOHANG) == None
+	
+
 	def seen(self):
 		self.lastseen=time()
+	
+	def readtobuffer(self):
+		if(self.buffer!=False):
+			try:  line = self.queue.get_nowait() # or q.get(timeout=.1)
+			except Empty: # No output
+				pass
+			else: # got line
+				self.buffer+=line
+	
 	def expired(self):
-		if(time()-self.lastseen>expiretime):
+		self.readtobuffer()
+		
+		if(time()-self.lastseen>self.expiretime):
 			return True
 		return False
+
+def enqueue_output(out, queue):
+    for line in iter(out.readline, ''):
+        queue.put(line)
+    out.close()
+
+expire_timer = False
+def check_expired():
+	global expire_timer
+	if(expire_timer):
+		expire_timer.cancel()
+		expire_timer=False
+	
+	global processes
+	newprocesses=[]
+	for proc in processes:
+		if not(proc.expired()):
+			newprocesses.append(proc)
+		else
+			del proc
+	processes=newprocesses
+	
+	if(len(processes)>0):
+		expire_timer = Timer(EXPIRE_RATE,check_expired)
+		expire_timer.start()
+		
+if(__name__=="__main__"):
+	p=process(('/var/apps/RamonaSt/ramonast/library/slowassprogram'),8,True)
+	sleep(1)
+	print(p.read())
+	sleep(5)
+	print(p.read())
+	sleep(15)
+	print(p.read())
